80cae27b62fd9bb43106913faffcbf049dbaf3d1
EC2 Default User <ec2-user@ip-172-31-52-163.ec2.internal>
2024-01-30 21:18:57 +0000

    Empty Pipeline

--- lib/cdk-pipeline-eb-demo-stack.ts
+++ lib/cdk-pipeline-eb-demo-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {  Stack, StackProps } from 'aws-cdk-lib';
 
-export class CdkPipelineEbDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkPipelineEbDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+       // How it will be built and synthesized
+       synth: new ShellStep('Synth', {
+         // Where the source can be found
+         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+         
+         // Install dependencies, build and run cdk synth
+         installCommands: ['npm i -g npm@latest'],
+         commands: [
+           'npm ci',
+           'npm run build',
+           'npx cdk synth'
+         ],
+       }),
+    });
+
+    // This is where we add the application stages
   }
 }
